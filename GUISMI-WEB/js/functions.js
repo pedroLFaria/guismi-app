3523730edae1c45101bcd4a6d8ef9e569aa5e71b
Universidade Federal de Santa Catarina <ufsc@ufscXXXX>
2019-04-12 14:24:59 -0300

    caixa de habilidades e botão

--- GUISMI-WEB/js/functions.js
+++ GUISMI-WEB/js/functions.js
@@ -1,26 +1,18 @@
 function abrirCaracteristicas() {
     document.getElementsByClassName('character-main-chracteristcs')[0].style.display = 'block';
     document.getElementsByClassName('character-main-inventario')[0].style.display = 'none';
-    document.getElementsByClassName('character-main-antecedentes')[0].style.display = 'none';
-    document.getElementsByClassName('character-main-habilidades')[0].style.display = 'none';  
-}
-function abrirHabilidades(){
-    document.getElementsByClassName('character-main-inventario')[0].style.display = 'none';
-    document.getElementsByClassName('character-main-chracteristcs')[0].style.display = 'none';
-    document.getElementsByClassName('character-main-antecedentes')[0].style.display = 'none';  
-    document.getElementsByClassName('character-main-habilidades')[0].style.display = 'block';
+    document.getElementsByClassName('character-main-antecedentes')[0].style.display = 'none'; 
 }
 function abrirAntecedentes(){
     document.getElementsByClassName('character-main-chracteristcs')[0].style.display = 'none';
     document.getElementsByClassName('character-main-inventario')[0].style.display = 'none';
     document.getElementsByClassName('character-main-antecedentes')[0].style.display = 'block';
-    document.getElementsByClassName('character-main-habilidades')[0].style.display = 'none';  
+
 }
 function abrirInventario(){
     document.getElementsByClassName('character-main-chracteristcs')[0].style.display = 'none';
     document.getElementsByClassName('character-main-inventario')[0].style.display = 'block';
     document.getElementsByClassName('character-main-antecedentes')[0].style.display = 'none';
-    document.getElementsByClassName('character-main-habilidades')[0].style.display = 'none';  
 }
 function modalTextBox(node) {
     node.style.display = "none";
