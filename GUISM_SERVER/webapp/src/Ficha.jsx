857e11b8e1ad7d58f5a95052d25808a766580ef9
pedro.luz <pedro.luz@ibratan.com.br>
2019-08-23 19:22:55 -0300

    Raca react

--- GUISM_SERVER/webapp/src/Ficha.jsx
+++ GUISM_SERVER/webapp/src/Ficha.jsx
@@ -1,15 +1,25 @@
+class Raca extends React.component{
+    constructor(props){
+        super(props)
+        this.state = {
+            racas : props.racas
+        }
+    }
+}
 class Ficha extends React.Component{
     constructor(props) {
         super(props)
-        this.state = {ficha:[], sistema:JSON.parse(sessionStorage.getItem("sistema"))}
-    }
+        this.state = {ficha:{
+            raca:{},
+            caminhos:{},
+            descendencias:{},
+            habilidades:{},
+            habitos:{},
+            idiomas:{}
+        }, sistema:JSON.parse(sessionStorage.getItem("sistema"))}
+    };
+
     componentDidMount(){
-        this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-    }
-    tick() {
         fetch("api/ficha/id/1", { method: "GET", Header: new Headers })
         .then(status)
         .then(json)
@@ -17,13 +27,16 @@ class Ficha extends React.Component{
             sessionStorage.setItem("ficha", JSON.stringify(data))
              this.setState({ficha : data});
         })
-  }
+    };
 
     render() {
         return (
             <div>
                 <h1>nome: {this.state.ficha.nomePersonagem}</h1>
-            </div>)
+                Raca<select value={this.state.ficha.raca.nomeRaca}></select>
+            </div>
+            )
+
     }
 }
 ReactDOM.render(<Ficha />, document.getElementById('ficha'))
