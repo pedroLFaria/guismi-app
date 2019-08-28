75fe570b34840067126a9e2d974487e41dfaead1
pedro.luz <pedro.luz@ibratan.com.br>
2019-08-28 12:14:43 -0300

    REACT

--- GUISM_SERVER/webapp/src/Ficha.jsx
+++ GUISM_SERVER/webapp/src/Ficha.jsx
@@ -9,10 +9,6 @@ class Raca extends React.component{
 class Ficha extends React.Component{
     constructor(props) {
         super(props)
-<<<<<<< HEAD
-        this.state = {ficha:{raca:{}, caminhos:[{}], descendencias:[{}], habilidades:[{}], habitos:[{}], idiomas:[{}], inventarios:[{}], patronos:[{}], situacoes:[{}]}, sistema:JSON.parse(sessionStorage.getItem("sistema"))}
-    }
-=======
         this.state = {ficha:{
             raca:{},
             caminhos:{},
@@ -22,8 +18,6 @@ class Ficha extends React.Component{
             idiomas:{}
         }, sistema:JSON.parse(sessionStorage.getItem("sistema"))}
     };
-
->>>>>>> 857e11b8e1ad7d58f5a95052d25808a766580ef9
     componentDidMount(){
         fetch("api/ficha/id/1", { method: "GET", Header: new Headers })
         .then(status)
@@ -38,15 +32,11 @@ class Ficha extends React.Component{
         return (
             <div>
                 <h1>nome: {this.state.ficha.nomePersonagem}</h1>
-<<<<<<< HEAD
                 <h1>raca: {this.state.ficha.raca.nomeRaca}</h1>
             </div>)
-=======
                 Raca<select value={this.state.ficha.raca.nomeRaca}></select>
             </div>
             )
-
->>>>>>> 857e11b8e1ad7d58f5a95052d25808a766580ef9
     }
 }
 ReactDOM.render(<Ficha />, document.getElementById('ficha'))
