f453e8b2a8589cbf4ed515c734853c3b119d0222
pedro.luz <pedro.luz@ibratan.com.br>
2019-08-29 12:46:17 -0300

    Tabela de habitos e iniciando o desenvolvimento da tela

--- GUISM_SERVER/webapp/src/Ficha.jsx
+++ GUISM_SERVER/webapp/src/Ficha.jsx
@@ -1,36 +1,31 @@
 class Habitos extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = { habitos : this.props.habitos}
-    }
-    /*
-    var habitosComponemt = props.habitos[0].nomeHabitos
-    return (
-        <div>
-        <h1>Habitos</h1>
-        <p>{habitosComponemt}</p>
-        </div>
-    )*/
-    
-
-    componentDidMount(){
-        this.setState({habitos : this.props.habitos})
-    };
- /*   retornaListaHabitos(){
-        console.log(this.state.habitos.map((habito)=> <p>{habito.nomHabito}<p>))
-    }*/
-
     render(){
-        
-        return <p>{this.props.habitos.map((habito) => <p>{habito.nomeHabito}</p>)}</p>
+        return (<table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>HABITOS</th>
+                            <th>Quantidade</th>
+                            <th>Dividido Por 5</th>
+                            <th>Dividido Por 10</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.habitos.map((habito) => 
+                        <tr>
+                            <td>{habito.nomeHabito}</td>
+                            <td>{habito.qtdFichaHabito}</td>
+                            <td>{habito.qtdFichaHabito/5}</td>
+                            <td>{habito.qtdFichaHabito/10}</td>
+                        </tr>)} 
+                    </tbody>
+                </table>)
     }
 }
 
 class Ficha extends React.Component{
     constructor(props) {
         super(props)
-        console.log(JSON.parse(sessionStorage.getItem("ficha")))
-        this.state = {ficha: JSON.parse(sessionStorage.getItem("ficha")) , sistema:JSON.parse(sessionStorage.getItem("sistema"))}
+        this.state = { ficha : JSON.parse(sessionStorage.getItem("ficha")) , sistema:JSON.parse(sessionStorage.getItem("sistema"))}
     };
     componentDidMount(){
         this.timerID = setInterval(() => this.tick(),1000);
@@ -39,30 +34,35 @@ class Ficha extends React.Component{
     tick(){
         fetch("api/ficha/id/1", { method: "GET", Header: new Headers })
         .then(status)
-        .then(json)
-        .then(data => {sessionStorage.setItem("ficha", JSON.stringify(data));this.setState({ ficha : data })})
-        
+        .then(response => response.json())
+        .then(data => {sessionStorage.setItem("ficha", JSON.stringify(data));this.setState({ ficha : data })})        
     }
 
     render() {
         const habitosConst = this.state.ficha.habitos
         return (
-            <div>
-                <h1>nome: {this.state.ficha.nomePersonagem}</h1>
-                <h1>raca: {this.state.ficha.raca.nomeRaca}</h1>
-                <Habitos
-                habitos = {habitosConst}
-                />
-            </div>
+            <fieldset className="scheduler-border form-group row">
+                <legend className="scheduler-border">Personagem </legend>
+                <div className="row">
+                    <div className="row col-md-12">
+                        <div className="col-md-2 inline">
+                            <span>Nome</span><br/>
+                            <label for="nome_pers_input" onDblClick="modalTextBox(this)" id="nome_pers">{this.state.ficha.nomePersonagem}</label>
+                            <input type="text" className="c form-control"  placeholder="" id="nome_pers_input"/>
+                        </div>
+                        <h1>raca: {this.state.ficha.raca.nomeRaca}</h1>
+                    </div>
+                    <Habitos
+                    habitos = {habitosConst}
+                    />
+                </div>
+            </fieldset>
             )
     }
 }
 
 fetch("api/ficha/id/1", { method: "GET", Header: new Headers })
         .then(status)
-        .then(json)
-        .then(data => {
-            sessionStorage.setItem("ficha", JSON.stringify(data))
-          
-        }).then(
-ReactDOM.render(<Ficha />, document.getElementById('ficha')))
+        .then(response => response.json())
+        .then(data => {sessionStorage.setItem("ficha", JSON.stringify(data))})
+        .then(ReactDOM.render(<Ficha />, document.getElementById('telaPersonagem')))
