cb059f128e30a46ff640726ec23b0275c56f658e
pedro.luz <pedro.luz@ibratan.com.br>
2019-09-16 09:17:17 -0300

    Criando um projeto REACT

--- GUISM_SERVER/tests/situacao/SituacaoResourceTest.java
+++
@@ -1,7 +0,0 @@
-package situacao;
-
-import org.junit.Ignore;
-
-@Ignore
-public class SituacaoResourceTest {
-}
--- GUISM_SERVER/webapp/cadastro/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "root", "");
-mysqli_set_charset($con, 'utf8');
-$db = mysqli_select_db($con, "guismi");
-
-if (!$con || !$db) echo mysqli_error($con); 
-?>
