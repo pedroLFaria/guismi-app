f32f3e812fe8a662cb3705bb92f5623243ba23da
pedro.luz <pedro.luz@ibratan.com.br>
2019-09-19 19:55:51 -0300

    Escolhe Ficha

---
+++ src/escolheFicha/EscolheFicha.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+class EscolheFicha extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            resumoFichas : []
+        }
+    }
+
+    componentDidMount() {
+        this.getSistema();
+        this.fetchResumoFichas();
+    }
+    fetchResumoFichas(){
+        fetch("http://localhost:9000/api/ficha/jogador", { method:"GET", Header : new Headers() })
+            .then(response => response.json())
+            .then(data=>{
+                let resumoFichas = data.map((ficha) =>{
+                    return (<div>
+                            <form action="" method="GET">
+                                <button className='ficha-sel' name='idFicha' value={ficha.idFicha}>
+                                    <div className="corpo">
+                                        <label className='botao-texto' type="submit" id={ficha.idFicha}>
+                                            <label>Nome:
+                                            </label>
+                                            <h3>{ficha.nomePersonagem}</h3>
+                                        </label>
+                                        <img src={ficha.img}/>
+                                    </div>
+                                </button>
+                            </form><br />
+                        </div>)
+                });
+                this.setState({resumoFichas:resumoFichas})
+            })
+    }
+    getSistema(){
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', ()=>{sessionStorage.setItem("sistema", xhr.responseText)});
+        xhr.open('GET','api/sistema');
+        xhr.send();
+    }
+
+    render () {
+        return(
+            <div>
+                {this.state.resumoFichas}
+            </div>
+        )
+    }
+}
+export { EscolheFicha }
