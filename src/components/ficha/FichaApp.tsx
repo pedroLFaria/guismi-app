70580c12375916bb94e08c9f172eed87b4d2021f
pedro.luz <pedro.luz@ibratan.com.br>
2019-10-18 11:06:58 -0300

    Passando conteúdo da aba ficha para o componente FichaApp

--- src/components/ficha/FichaApp.tsx
+++ src/components/ficha/FichaApp.tsx
@@ -1,5 +1,81 @@
-class FichaApp {
+import React from "react";
+import Ficha from "./Ficha";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {RacaApp} from "../raca/RacaApp";
+import {CaminhosApp} from "../caminho/CaminhosApp";
+import DescendenciasApp from "../descendencia/DescendenciasApp";
+import TabelaDeAtributosApp from "./TabelaDeAtributosApp";
+import {HabitoApp} from "../habito/HabitoApp";
 
+interface Props {
+    ficha:Ficha
+    updateFicha(arg0:Ficha):void
 }
 
-export {FichaApp}
+interface State {
+    ficha:Ficha
+}
+
+export default class FichaApp extends React.Component<Props, State>{
+    constructor(props:Props) {
+        super(props);
+        this.state ={
+            ficha:this.props.ficha
+        }
+    }
+
+    componentDidUpdate(prevProps: Readonly<Props>) {
+        if(JSON.stringify(prevProps.ficha) !== JSON.stringify(this.props.ficha))
+            this.setState({
+                ficha:this.props.ficha
+            })
+    }
+
+    render(){
+        const ficha = this.state.ficha;
+        return(
+            <div>
+                <Row>
+                    <legend>Personagem</legend>
+                </Row>
+                <Row>
+                    <Col md={3}>
+                        <RacaApp
+                            updateFicha={this.props.updateFicha}
+                            ficha={ficha}
+                        />
+                    </Col>
+                    <Col>
+                        <CaminhosApp
+                            updateFicha={this.props.updateFicha}
+                            ficha={ficha}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <DescendenciasApp
+                            ficha={ficha}
+                        />
+                    </Col>
+                    <Col>
+                        espaço vazio
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <TabelaDeAtributosApp
+                            ficha={ficha}
+                        />
+                    </Col>
+                    <Col>
+                        <HabitoApp
+                            ficha={ficha}
+                        />
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
--- src/personagem/FichaPersonagem.tsx
+++ src/personagem/FichaPersonagem.tsx
@@ -3,22 +3,17 @@ import queryString from 'query-string'
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { NomeApp } from "../components/ficha/NomeApp";
-import { RacaApp } from "../components/raca/RacaApp";
-import { CaminhosApp } from "../components/caminho/CaminhosApp";
-import { HabitoApp } from "../components/habito/HabitoApp";
+import {NomeApp} from "../components/ficha/NomeApp";
 import Ficha from "../components/ficha/Ficha";
 import Sistema from "../components/sistema/Sistema";
 import MyHeaders from "../_services/MyHeaders";
-import TabelaDeAtributosApp from "../components/ficha/TabelaDeAtributosApp";
 import IdadeApp from "../components/ficha/IdadeApp";
 import NivelPersonagem from "../components/ficha/NivelPersonagem";
 import ExperienciaApp from "../components/ficha/ExperienciaApp";
-import DescendenciasApp from "../components/descendencia/DescendenciasApp";
-import { Nav, NavItem, TabContainer, TabContent } from "react-bootstrap";
+import {Nav, NavItem, TabContainer, TabContent, TabPane} from "react-bootstrap";
 import Spinner from 'react-bootstrap/Spinner'
-import { TabPane } from 'react-bootstrap';
 import HabilidadesApp from '../components/habilidade/HabilidadesApp';
+import FichaApp from "../components/ficha/FichaApp";
 
 interface State {
     ficha: Ficha
@@ -45,11 +40,11 @@ export default class FichaPersonagem extends React.Component<State, State> {
         })
             .then(response => response.json())
             .then(data => {
-                this.setState({ ficha: data as Ficha })
+                this.setState({ficha: data as Ficha})
             });
     };
 
-    atualizaFicha(newFicha: Ficha) {
+    updateFicha(newFicha: Ficha) {
         this.setState({
             ficha: newFicha,
             loading: true
@@ -57,7 +52,7 @@ export default class FichaPersonagem extends React.Component<State, State> {
         Ficha.update(this.state.ficha).then(response => {
             if (response.ok) {
                 console.log("Ficha atualizada com sucesso.");
-                this.setState({ loading: false });
+                this.setState({loading: false});
                 return true
             } else {
                 console.log("Status " + response.statusText);
@@ -69,29 +64,29 @@ export default class FichaPersonagem extends React.Component<State, State> {
     render() {
         const ficha = this.state.ficha;
         return (
-            <Container fluid >
+            <Container fluid>
                 <Row>
                     <Col md={3}>
                         <NomeApp
-                            updateFicha={this.atualizaFicha.bind(this)}
+                            updateFicha={this.updateFicha.bind(this)}
                             ficha={ficha}
                         />
                     </Col>
                     <Col md={3}>
                         <IdadeApp
-                            updateFicha={this.atualizaFicha.bind(this)}
+                            updateFicha={this.updateFicha.bind(this)}
                             ficha={ficha}
                         />
                     </Col>
                     <Col md={3}>
                         <NivelPersonagem
-                            updateFicha={this.atualizaFicha.bind(this)}
+                            updateFicha={this.updateFicha.bind(this)}
                             ficha={ficha}
                         />
                     </Col>
                     <Col md={3}>
                         <ExperienciaApp
-                            updateFicha={this.atualizaFicha.bind(this)}
+                            updateFicha={this.updateFicha.bind(this)}
                             ficha={ficha}
                         />
                     </Col>
@@ -106,12 +101,12 @@ export default class FichaPersonagem extends React.Component<State, State> {
                         <NavItem>
                             <Nav.Link eventKey={"ficha"}>
                                 Ficha
-                        </Nav.Link>
+                            </Nav.Link>
                         </NavItem>
                         <NavItem>
                             <Nav.Link eventKey={"quadroDeBatalha"}>
                                 Quadro de Batalha
-                        </Nav.Link>
+                            </Nav.Link>
                         </NavItem>
                         <NavItem>
                             <Nav.Link
@@ -126,50 +121,19 @@ export default class FichaPersonagem extends React.Component<State, State> {
                         <Spinner
                             animation="border"
                             variant="primary"
-                            style={{ "float": "right", "marginLeft": "auto", "display": this.state.loading ? "inline-block" : "none" }}
+                            style={{
+                                "float": "right",
+                                "marginLeft": "auto",
+                                "display": this.state.loading ? "inline-block" : "none"
+                            }}
                         />
                     </Nav>
                     <TabContent>
                         <TabPane eventKey={"ficha"}>
-                            <Row>
-                                <legend>Personagem</legend>
-                            </Row>
-                            <Row>
-                                <Col md={3}>
-                                    <RacaApp
-                                        updateFicha={this.atualizaFicha.bind(this)}
-                                        ficha={ficha}
-                                    />
-                                </Col>
-                                <Col>
-                                    <CaminhosApp
-                                        updateFicha={this.atualizaFicha.bind(this)}
-                                        ficha={ficha}
-                                    />
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <DescendenciasApp
-                                        ficha={ficha}
-                                    />
-                                </Col>
-                                <Col>
-                                    espaço vazio
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <TabelaDeAtributosApp
-                                        ficha={ficha}
-                                    />
-                                </Col>
-                                <Col>
-                                    <HabitoApp
-                                        ficha={ficha}
-                                    />
-                                </Col>
-                            </Row>
+                            <FichaApp
+                                ficha={ficha}
+                                updateFicha={this.updateFicha.bind(this)}
+                            />
                         </TabPane>
                         <TabPane eventKey={"quadroDeBatalha"}>
                             QUADRO DE BATALHA
@@ -180,7 +144,7 @@ export default class FichaPersonagem extends React.Component<State, State> {
                         <TabPane eventKey={"habilidades"}>
                             <HabilidadesApp
                                 ficha={ficha}
-                                updateFicha={this.atualizaFicha.bind(this)}
+                                updateFicha={this.updateFicha.bind(this)}
                             />
                         </TabPane>
                     </TabContent>
@@ -190,4 +154,4 @@ export default class FichaPersonagem extends React.Component<State, State> {
     }
 }
 
-export { FichaPersonagem }
+export {FichaPersonagem}
